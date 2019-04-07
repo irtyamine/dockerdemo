a4e27050ca36758a24617dc709c55e2efa9137d9
Yaser <adel.mehrban@gmail.com>
2019-04-07 20:54:08 +1000

    added docker file

--- run.bat
+++ run.bat
@@ -2,4 +2,6 @@ ECHO OFF
 
 ECHO nodejs image
 
-docker run --platform=linux -it --rm --name node -d -v %CD%:/src -w /src -p 8080:3000 node:11.9.0-alpine node app.js
+docker build --platform=linux -t nodejs-app .
+
+docker run --platform=linux --rm -p 8080:3000 -d nodejs-app
