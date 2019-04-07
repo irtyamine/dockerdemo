fb56efcfbcbb7f7cba5eafbd3e7f7de393be200e
Yaser <adel.mehrban@gmail.com>
2019-04-07 17:11:12 +1000

    Added basic

---
+++ run.bat
@@ -0,0 +1,5 @@
+ECHO OFF
+
+ECHO Running the docker ubunto example
+
+docker run -it --platform=linux ubuntu /bin/bash
---
+++ run.sh
@@ -0,0 +1,5 @@
+#! /bin/sh
+
+echo Running the docker ubuntu example
+
+docker run -it --platform=linux ubuntu /bin/bash
